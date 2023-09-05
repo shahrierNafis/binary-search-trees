89af39d75b2c734fce4a28477c241e39a1299447
Shahrier Nafis <102902752+shahrierNafis@users.noreply.github.com>
2023-09-05 15:49:59 +0600

    Add insert and deleteNode functions

--- Tree.js
+++ Tree.js
@@ -1,25 +1,4 @@
 import Node from "./Node.js";
-/**
- * Function to create a binary search tree from an array of numbers
- * @param {Array} array - The input array of numbers
- * @returns {Object} - The root node of the binary search tree
- */
-function create(array) {
-  // Remove duplicates and sort the array in ascending order
-  array = Array.from(
-    new Set(
-      array.sort(function (a, b) {
-        return a - b;
-      })
-    )
-  );
-
-  let start = 0;
-  let end = array.length - 1;
-  return {
-    root: buildTree(array, start, end),
-  };
-}
 
 /**
  * Recursive function to build a binary search tree from a sorted array
@@ -40,5 +19,119 @@ function buildTree(array, start, end) {
   root.right = buildTree(array, mid + 1, end);
 
   return root;
+}
+
+/**
+ * Function to create a binary search tree from an array of numbers
+ * @param {Array} array - The input array of numbers
+ * @returns {Object} - The root node of the binary search tree
+ */
+function create(array) {
+  // Remove duplicates and sort the array in ascending order
+  array = Array.from(
+    new Set(
+      array.sort(function (a, b) {
+        return a - b;
+      })
+    )
+  );
+
+  const start = 0;
+  const end = array.length - 1;
+  const root = buildTree(array, start, end);
+
+  /**
+   * Inserts a node with the given data into the binary tree.
+   * @param {number} data - The data to be inserted.
+   */
+  function insert(data) {
+    // find the leaf node set it
+    find(data).node.data = data;
+  }
+
+  /**
+   * Deletes a node from a binary tree.
+   *
+   * @param {number} data - The value of the node to be deleted.
+   */
+  function deleteNode(data) {
+    // find the node and its parent
+    let { node, parentNode } = find(data);
+
+    // if the node is a leaf
+    if (!node.left && !node.right) {
+      if (parentNode.left === node) {
+        parentNode.left = null; // if the node is left to its parent
+      } else {
+        parentNode.right = null;
+      }
+    }
+    // if the node only has a left child
+    else if (node.left && !node.right) {
+      if (parentNode.data > node.left.data) {
+        parentNode.left = node.left;
+      } else {
+        parentNode.right = node.left;
+      }
+    }
+    // if the node only has a right child
+    else if (!node.left && node.right) {
+      if (parentNode.data > node.right.data) {
+        parentNode.left = node.right;
+      } else {
+        parentNode.right = node.right;
+      }
+    }
+    // if the node has two children
+    else {
+      // find the inorder successor
+      let successor = node.right;
+      while (successor.left) {
+        successor = successor.left;
+      }
+      // delete the inorder successor
+      deleteNode(successor.data);
+      // replace the node's data with the inorder successor's data
+      node.data = successor.data;
+    }
+  }
+  /**
+   * Finds a node in the tree with the specified data.
+   * @param {number} data - The data to search for.
+   * @returns {Object} - An object containing the found node and its parent node.
+   *                    If the node is not found, the last visited node is returned.
+   */
+  function find(data) {
+    // Start at the root node
+    let node = root;
+    let parentNode = null;
+
+    // Traverse the tree until reaching a leaf node
+    while (node.left || node.right) {
+      // If the current node's data is greater than the search data, go to the left child node
+      if (node.data > data) {
+        parentNode = node;
+        node = node.left;
+      }
+      // If the current node's data is less than the search data, go to the right child node
+      else if (node.data < data) {
+        parentNode = node;
+        node = node.right;
+      }
+      // If the current node's data is equal to the search data, return the current node
+      else {
+        break;
+      }
+    }
+
+    return { node, parentNode };
+  }
+  return {
+    get root() {
+      return root;
+    },
+    insert,
+    deleteNode,
+  };
 }
 export default { create };
--- index.js
+++ index.js
@@ -2,7 +2,23 @@ import Tree from "./Tree.js";
 import { prettyPrint } from "./prettyPrint.js";
 
 // Create a binary search tree from the given array
-let tree = Tree.create([1, 2, 5, 6, 7, 7, 6, 6, 4, 5, 9, 565, 3, 8, 4]).root;
+let tree = Tree.create([1, 2, 5, 6, 7, 7, 6, 6, 4, 5, 9, 565, 3, 8, 4]);
 
 // Print the binary search tree
-prettyPrint(tree);
+prettyPrint(tree.root);
+
+tree.insert(10);
+
+prettyPrint(tree.root);
+
+tree.deleteNode(1);
+
+prettyPrint(tree.root);
+
+tree.deleteNode(3);
+
+prettyPrint(tree.root);
+
+tree.deleteNode(5);
+
+prettyPrint(tree.root);
